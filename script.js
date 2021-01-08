ad284f9bb471212f1c330b214231782c12fb32be
Facundo <facu2000veronelli@outlook.com>
2021-01-08 19:54:15 -0300

    init page

---
+++ script.js
@@ -0,0 +1,90 @@
+const banners = document.querySelector('#banners')
+const bannerrs = document.querySelectorAll(".banner")
+const back = document.getElementById('btn-back')
+const next = document.getElementById('btn-next')
+const move = document.getElementById('move')
+
+var index = 0
+var presed = false
+
+
+const WinWid = window.innerWidth
+const bannerWidth = WinWid
+bannerrs.forEach(banner=>{
+
+    banner.setAttribute('style',`width:${bannerWidth}px`)
+
+})
+
+back.onclick = ()=>{
+
+    
+    banners.style.transition = ".5s"
+    if(index>=0){
+
+        return
+
+    }
+    banners.style.transform = `translateX(${++index * 110}%)`
+    console.log(banners.style.transform)
+
+    presed = true
+}
+
+next.onclick = ()=>{
+
+    
+    banners.style.transition = ".5s"
+    if(-index>=bannerrs.length-1){
+
+        return
+
+    }
+    banners.style.transform = `translateX(${--index * 110}%)`
+    console.log(banners.style.transform)
+    presed = true
+
+}
+
+function reportWindowsSize(){
+
+    const WinWid = window.innerWidth
+    const bannerWidth = WinWid
+    bannerrs.forEach(banner=>{
+
+        banner.setAttribute('style',`width:${bannerWidth}px`)
+
+    })
+}
+window.addEventListener('scroll',()=>{
+
+    
+console.log(move.offsetTop*10, Math.round(window.scrollY))
+
+    if(move.offsetTop*10 < window.scrollY){
+
+        move.style.transform = "translateX(0%)"
+        move.style.opacity = "1"
+
+    }
+
+})
+setInterval(()=>{
+
+    if(presed == false){
+
+        banners.style.transition = "1s"
+        if(-index>=bannerrs.length-1){
+    
+            index = 1
+    
+        }
+        
+        banners.style.transform = `translateX(${--index * 110}%)`
+
+    }
+    presed = false
+
+},5000)
+
+window.onresize = reportWindowsSize
